04cbc977d15fd7ba56df1aeece097d96797cbe27
jgsnto <jgsnto.sd@gmail.com>
2023-12-21 20:24:08 +0100

    Starting commit w/ structure for first section

---
+++ Data_Structures/Basic_Concepts/src/Arrays_Basic.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Arrays_Basic.cpp
+// Author      : José
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Worlds!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
